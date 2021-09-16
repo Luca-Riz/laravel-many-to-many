e1d0fccdd253014ceaa54cd3f43738ab98dbe77b
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-09-16 15:28:50 +0200

    add tags tableSeeder

--- database/seeds/TagSeeder.php
+++ database/seeds/TagSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Tag;  //mi serve per parlare con db (foreach $seedTags as $tag)
 
 class TagSeeder extends Seeder
 {
@@ -11,6 +13,17 @@ class TagSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $seedTags = ['Front End', 'Back End', 'Database', 'User Experience'];
+
+        // $slug = Str::slug('Laravel 5 Framework', '-');
+
+        foreach ($seedTags as $tag) {
+            $newTag = new Tag();
+            
+            $newTag->name = $tag;
+            $newTag->slug = Str::slug($tag, '-');
+
+            $newTag->save();
+        }
     }
 }
