f62dcb261cd0322fa00c4350f4ee85fc57805aba
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-09-16 17:30:16 +0200

    add category and tags on show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -3,6 +3,18 @@
 @section('content')
     <div class="container">
       <h2>Dettagli del prodotto</h2>
+
+      {{-- parte tags --}}
+      <div class="my-2">
+        <span class="badge badge-secondary">Category:</span>
+        @if (isset($post->category->name))
+          <span class="badge badge-warning"> {{$post->category->name}} </span>
+        @else
+          <span>{{'Non si sono tag per questo post'}}</span>
+        @endif
+      </div>
+      {{-- fine parte tags --}}
+
       <div class="card">
         <div class="card-header">
           {{$post->title}}
@@ -12,6 +24,18 @@
           <p class="card-text">{{$post->content}}</p>
         </div>
       </div>
+
+      {{-- parte tags --}}
+      <div class="mt-2">
+        <span class="badge badge-secondary">Tags:</span>
+        @forelse ($post->tags as $tag)
+        <span class="badge badge-warning"> {{$tag->name}} </span>
+        @empty
+        <span>{{'Non si sono tag per questo post'}}</span>
+        @endforelse
+      </div>
+      {{-- fine parte tags --}}
+
       <div class="mt-2">
         <a href="{{route('admin.posts.index')}}" class="btn btn-primary">Torna indietro</a>
       </div>
